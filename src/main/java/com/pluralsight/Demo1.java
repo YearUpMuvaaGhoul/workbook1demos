c09f91403ea9549dd23f24c5dcbae5a911c647b7
LexxMatula <alexamatula@gmail.com>
2024-09-26 15:42:24 -0400

    finished math vacation time watching TV

--- src/main/java/com/pluralsight/Demo1.java
+++ src/main/java/com/pluralsight/Demo1.java
@@ -4,5 +4,31 @@ public class Demo1 {
 
     public static void main(String[] args){
         System.out.println("Hello World!");
+
+        char status = 'l';
+        int identifier = 1;
+        String name = "Lexx";
+
+
+        String greeting = "Hello " + name;
+        //String greeting = "Hello " + Lexx;
+          //String greeting = "Hello Lexx";
+
+        int numberOfDaysPerWeekWatchesTV = 5;
+        int minutesPerDayOnDayWatchedOnAverage = 120;
+
+        int minutesPerWeek = numberOfDaysPerWeekWatchesTV * minutesPerDayOnDayWatchedOnAverage;
+        int minutesPerYear = minutesPerWeek * 52;
+
+        int hoursPerYear = minutesPerYear / 60;
+
+        int hoursInWorkWeek = 40;
+
+        int workWeeksPerYearWatchingTV = hoursPerYear / hoursInWorkWeek;
+
+        System.out.println("If " + name + " did not watch TV, it would be like a " + workWeeksPerYearWatchingTV + " week vacation every year.");
+
+
+        System.out.println(greeting);
     }
 }
