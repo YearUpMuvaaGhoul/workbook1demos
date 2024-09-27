00828cdc2741f8e1c6247aabe3536715f59ef635
LexxMatula <alexamatula@gmail.com>
2024-09-27 11:52:25 -0400

    made new math problems

--- src/main/java/com/pluralsight/Demo1.java
+++ src/main/java/com/pluralsight/Demo1.java
@@ -2,7 +2,7 @@ package com.pluralsight;
 
 public class Demo1 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello World!");
 
         char status = 'l';
@@ -12,7 +12,7 @@ public class Demo1 {
 
         String greeting = "Hello " + name;
         //String greeting = "Hello " + Lexx;
-          //String greeting = "Hello Lexx";
+        //String greeting = "Hello Lexx";
 
         int numberOfDaysPerWeekWatchesTV = 5;
         int minutesPerDayOnDayWatchedOnAverage = 120;
@@ -28,8 +28,11 @@ public class Demo1 {
 
         System.out.println("If " + name + " did not watch TV, it would be like a " + workWeeksPerYearWatchingTV + " week vacation every year.");
 
-            // Math problem for solving for how many vacation weeks she would have, if she didn't watch TV;
+        // Math problem for solving for how many vacation weeks she would have, if she didn't watch TV;
 
         System.out.println(greeting);
+
     }
+
 }
+
